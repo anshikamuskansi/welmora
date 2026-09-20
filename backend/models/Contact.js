4f6c7198ca8e2ecdf743ff9651c1256887211708
Anshika Singh <anshikamuskansingh@gmail.com>
2026-09-20 18:38:31 +0530

    Add business field to contact schema

--- backend/models/Contact.js
+++ backend/models/Contact.js
@@ -9,6 +9,7 @@ const mongoose = require("mongoose");
 const contactSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
+    
     business: { type: String, required: true, trim: true },
     email: { type: String, required: true, trim: true, lowercase: true },
     message: { type: String, required: true },
